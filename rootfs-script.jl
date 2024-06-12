92601df17edb403a9889d7029fcfed47b370ef69
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2024-06-12 08:55:59 +0000

    add sandbox
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

---
+++ rootfs-script.jl
@@ -0,0 +1,42 @@
+using RootfsUtils: parse_build_args, upload_gha, test_sandbox
+using RootfsUtils: debootstrap
+using RootfsUtils: root_chroot
+
+args         = parse_build_args(ARGS, @__FILE__)
+arch         = args.arch
+archive      = args.archive
+image        = args.image
+
+packages = [
+    "bash",
+    "build-essential",
+    "coreutils",
+    "ca-certificates",
+    "curl",
+    "gnupg",
+    "git",
+    "locales",
+    "localepurge",
+]
+
+artifact_hash, tarball_path, = debootstrap(arch, image; archive, packages) do rootfs, chroot_ENV
+    my_chroot(args...) = root_chroot(rootfs, "bash", "-eu", "-o", "pipefail", "-c", args...; ENV=chroot_ENV)
+
+    cmd = """
+    set -Eeu -o pipefail
+    curl -fsSL http://build.openmodelica.org/apt/openmodelica.asc | gpg --dearmor -o /usr/share/keyrings/openmodelica-keyring.gpg
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/openmodelica-keyring.gpg] \
+        https://build.openmodelica.org/apt \
+        bullseye \
+        stable" | tee /etc/apt/sources.list.d/openmodelica.list
+
+    apt update
+    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends omc
+    rm -rf /var/lib/apt/lists/*
+    """
+    my_chroot(cmd)
+end
+
+@info artifact_hash
+@info tarball_path
+test_sandbox(artifact_hash)
